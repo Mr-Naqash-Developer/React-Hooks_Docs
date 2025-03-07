e4ef9a7cd347271af7d034af0b53e516ed78f0ff
Mr.Naqash.Developer <mr.naqash.developer@gmail.com>
2025-03-07 15:33:30 +0500

    I've complete useEffect step1 and step2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import UseState from './components/UseState'
+import UseEffect from './components/UseEffect'
 
 const App = () => {
   return (
     <>
       {/* <UseState /> */}
+      <UseEffect />
     </>
   )
 }
--- src/components/UseEffect.jsx
+++ src/components/UseEffect.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+
+function VideoPlayer({ src, isPlaying }) {
+  const ref = useRef(null)
+
+  useEffect(() => {
+    if (isPlaying) {
+      console.log('Calling video.play()')
+      ref.current.play()
+    } else {
+      console.log('Calling video.pause()')
+      ref.current.pause()
+    }
+  }, [isPlaying])
+
+  return <video className='w-[1000px] rounded-[18px] border-gray-900 border-[2px] ' src={src} ref={ref} loop playsInline />
+}
 
 const UseEffect = () => {
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [text, setText] = useState('')
   return (
-    <div>
-      
-    </div>
+      <div className='flex justify-center items-center flex-col-reverse gap-8 h-screen'>
+        <input className='border border-black' type={text} onChange={e => setText(e.target.value)} />
+        <button className='bg-red-600 py-2 px-6 text-white text-2xl rounded-[50px] w-[150px] flex justify-center items-center ' onClick={() => setIsPlaying(!isPlaying)}>
+          {isPlaying ? 'Pause' : 'Play'}
+        </button>
+        <VideoPlayer isPlaying={isPlaying} src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4" />
+      </div>
   )
 }
 
